f38bd3b93c41ac8a878ce73a5ef2bb5caefd2479
Suchanon Jaiwang <suchanon@gopomelo.com>
2023-02-08 11:26:13 +0700

    Add custom component

--- src/StateWarningPlugin.js
+++ src/StateWarningPlugin.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FlexPlugin } from '@twilio/flex-plugin';
-import FeatherCorpTheme  from './assets/FeatherCorpTheme';
+import FeatherCorpTheme from './assets/FeatherCorpTheme';
+import QuoteComponent from './components/Quote/Quote';
+
 const PLUGIN_NAME = 'StateWarningPlugin';
 
 export default class StateWarningPlugin extends FlexPlugin {
@@ -14,6 +16,16 @@ export default class StateWarningPlugin extends FlexPlugin {
     flex.MainHeader.defaultProps.logoUrl = 'https://tangerine-toad-5117.twil.io/assets/feathercorp-logo-white.svg'
 
     // apply theme
-    manager.updateConfig({ colorTheme : FeatherCorpTheme});
+    manager.updateConfig({ colorTheme: FeatherCorpTheme });
+
+    // remove default components
+    flex.NoTasksCanvas.Content.remove('first-line');
+    flex.NoTasksCanvas.Content.remove('second-line');
+    flex.NoTasksCanvas.Content.remove('hint');
+
+    // add our quote component
+    flex.NoTasksCanvas.Content.add(<QuoteComponent key="qotd" />, {
+      sortOrder: -1
+    });
   }
 }
