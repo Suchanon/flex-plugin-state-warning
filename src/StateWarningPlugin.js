1ebc9be800dfeb7bec42198c45addd4ee1f6d7d5
Suchanon Jaiwang <suchanon@gopomelo.com>
2023-02-10 11:07:11 +0700

    Add CRM

--- src/StateWarningPlugin.js
+++ src/StateWarningPlugin.js
@@ -24,8 +24,24 @@ export default class StateWarningPlugin extends FlexPlugin {
     flex.NoTasksCanvas.Content.remove('hint');
 
     // add our quote component
-    flex.NoTasksCanvas.Content.add(<QuoteComponent key="qotd" />, {
-      sortOrder: -1
-    });
+    // flex.NoTasksCanvas.Content.add(<QuoteComponent key="qotd" />, {
+    //   sortOrder: -1
+    // });
+
+    manager.strings.TaskHeaderLine =
+      '{{task.attributes.account_data.first_name}} ' +
+      '{{task.attributes.account_data.last_name}}';
+    manager.strings.TaskLineCallReserved = 'SLA: {{task.attributes.account_data.service_level}}';
+
+    //Add CRM view
+    flex.CRMContainer.defaultProps.uri= 'https://duckycrm-7409-dev.twil.io/index'
+    const crmUrl = 'https://duckycrm-7409-dev.twil.io';
+    flex.CRMContainer.defaultProps.uriCallback = (task) => {
+      if(task) {
+        return `${crmUrl}/profile?id=` + task.attributes.account_number;
+      } else {
+        return `${crmUrl}/index`;
+      }
+    };
   }
 }
